e257734582aadecb46f125f5ff8c470197223c3d
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2024-05-17 16:55:13 +0200

    parte 2 WIP

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,56 +1,60 @@
 package com.example;
 
 import com.example.algoritmos.parte1;
+import com.example.algoritmos.parte2;
 
+@SuppressWarnings("unused")
 public class App {
+
     public static void main(String[] args) {
         ///// PARTE1/////
 
-        int m = 5;
-        int n = 5;
-        int[][] matrix = parte1.generateOrderedMatrix(m, n);
+        // int m = 5;
+        // int n = 5;
+        // int[][] matrix = parte1.generateOrderedMatrix(m, n);
         // int[] result = parte1.linearMatSearch(matrix, matrix[3][4]);
         // int[] result = parte1.successfulMatSearch(matrix, matrix[3][4]);
         // int[] result = parte1.binaryMatSearch(matrix, matrix[3][4]);
-        int[] result = parte1.binaryMatRecursiveSearch(matrix, matrix[3][4], 0, (m * n) - 1, 0);
-        System.out.printf("%s;%s;%s;%s\n", "Descripcion", "Talla", "Iteraciones",
-                "Resultado");
-        System.out.printf("%s;%d;%d;%s\n", "linearMatSearchMejor", m * n, result[2],
-                "[" + result[0] + "," + result[1] + "]");
+        // int[] result = parte1.binaryMatRecursiveSearch(matrix, matrix[3][4], 0, (m *
+        // n) - 1, 0);
+        // System.out.printf("%s;%s;%s;%s\n", "Descripcion", "Talla", "Iteraciones",
+        // "Resultado");
+        // System.out.printf("%s;%d;%d;%s\n", "linearMatSearchMejor", m * n, result[2],
+        // "[" + result[0] + "," + result[1] + "]");
+
+        /// PREGUNTAS///
+        // 1.1.1
+        // EL coste temporal es n^2 ya que en número de iteraciones es m*n
+
+        // 1.1.2
+        // 50*50=2500
+
+        // 1.2.1
+        //
+
+        // 1.2.2
+        // Si porque el algoritmo de detiene en cuanto encuentra el valor que busca y no
+        // tiene la necesidad de recorrer toda la matriz
+
+        // 1.3.1
+        // Mejor que sea el del medio, peor que sea uno de los extremos (primer o ultimo
+        // elemento de la array)
+
+        // 1.3.2
+        // Realizara solo una iteracion ya que el algoritmo empieza a buscar por el
+        // elemento del centro.
+
+        // 1.3.3
+        // 2500 iteraciones
+
+        // 1.4.1
+        // El resultado es el mismo que el anterior
+
+        // 1.4.2
+        // Nos da stack overflow
+
+        ///// PARTE2/////
+        parte2.medirTiempo();
 
     }
-
-    /// PREGUNTAS///
-    // 1.1.1
-    // EL coste temporal es n^2 ya que en número de iteraciones es m*n
-
-    // 1.1.2
-    // 50*50=2500
-
-    // 1.2.1
-    //
-
-    // 1.2.2
-    // Si porque el algoritmo de detiene en cuanto encuentra el valor que busca y no
-    // tiene la necesidad de recorrer toda la matriz
-
-    // 1.3.1
-    // Mejor que sea el del medio, peor que sea uno de los extremos (primer o ultimo
-    // elemento de la array)
-
-    // 1.3.2
-    // Realizara solo una iteracion ya que el algoritmo empieza a buscar por el
-    // elemento del centro.
-
-    // 1.3.3
-    // 2500 iteraciones
-
-    // 1.4.1
-    // El resultado es el mismo que el anterior
-
-    // 1.4.2
-    // Nos da stack overflow
-
-    ///// PARTE2/////
-
 }
--- src/main/java/com/example/algoritmos/parte2.java
+++ src/main/java/com/example/algoritmos/parte2.java
@@ -1,5 +1,80 @@
 package com.example.algoritmos;
 
+import java.util.function.Function;
+
 public class parte2 {
+
+    private static int randomNum(int min, int max) {
+        return min + (int) (Math.random() * ((max - min) + 1));
+    }
+
+    public static int[] generateOrderedArray(int n) {
+        int min = 0;
+        int max = 100;
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = randomNum(min, max);
+        }
+        return arr;
+
+    }
+
+    public static int[] linealSearchIterative(int[] array, int valor) {
+        int pos = -1;
+        int iterations = 0;
+        int[] result = { pos, iterations };
+        boolean found = false;
+        for (int i = 0; i < array.length && !found; i++) {
+            iterations++;
+            if (valor == array[i]) {
+                pos = i;
+                found = true;
+            }
+        }
+        result[0] = pos;
+        result[1] = iterations;
+        return result;
+    }
+
+    public static void medirTiempo() {
+        final int INIT_SIZE = 100000, // Talla inicial
+                MAX_SIZE = 1000000, // Talla final
+                STEP = 100000, // A cada medida, incrementamos la talla en STEP
+                REPS = 1000, // Repeticiones para una media más fiable
+                ARRAY_SIZE = 100; // Tamaño del array
+
+        int[] array = generateOrderedArray(ARRAY_SIZE);
+        int[] result;
+        int arraySize, vBest, vWorst;
+        long timeEnd, timeStart, tBest, tWorst, tAvr;
+
+        System.out.printf("%10s;%10s;%10s;%10s\n", "Talla", "Mejor", "Peor",
+                "Promedio");
+
+        for (int size = INIT_SIZE; size <= MAX_SIZE; size += STEP) {
+            array = generateOrderedArray(size);
+            arraySize = array.length;
+
+            vBest = array[0];
+            vWorst = array[arraySize - 1];
+
+            timeStart = System.nanoTime();
+            for (int rep = 0; rep < REPS; rep++) {
+                result = linealSearchIterative(array, vBest);
+            }
+            timeEnd = System.nanoTime();
+            tBest = (timeEnd - timeStart) / REPS;
+            // Implementar el cálculo para el peor valor
+            timeStart = System.nanoTime();
+            for (int rep = 0; rep < REPS; rep++) {
+                result = linealSearchIterative(array, vWorst);
+            }
+            timeEnd = System.nanoTime();
+            tWorst = (timeEnd - timeStart) / REPS;
+            // Implementar el cálculo para el valor promedio
+            tAvr = (tBest + tWorst) / 2;
+            System.out.printf("%10d;%10d;%10d;%10d\n", size, tBest, tWorst, tAvr);
+        }
+    }
 
 }
